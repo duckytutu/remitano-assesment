a2339375a33090bee2b1c0a6ca514c67ddc2bcfd
TungNguyen <namlv@smartosc.com>
2022-07-30 22:35:23 +0700

    feat: post movie api and incomplete getAll movie api

--- server/src/movies/movies.controller.ts
+++ server/src/movies/movies.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { CreateMovieDto } from 'src/users/dto/create-movie.dto';
+import { CreateMovieDto } from 'src/movies/dto/create-movie.dto';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
@@ -12,8 +12,10 @@ export class MoviesController {
   }
 
   @Get()
-  findAll() {
-    return this.moviesService.findAll();
+  async findAll(@Param('userId') userId: number) {
+    const data = await this.moviesService.findAll();
+    //raw query from db: SELECT m.id, m.movieTitle, m.movieDescription, m.movieUrl, m.createdAt, u.username FROM Movies m left join Users u on m.userId = u.id order by m.createdAt desc
+    return data;
   }
 
   @Get(':id')
